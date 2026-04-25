b579a69fe2cbb65e599d00a45d22be40d36ee247
leandro <leandrocabral321@gmail.com>
2026-04-25 15:10:00 -0300

    jest setup

---
+++ frontend/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  clearMocks: true,
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default createJestConfig(config);
